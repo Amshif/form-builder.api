f94da136207c0d1fd203f03b79d5fdcc27bae9dc
Amshif <you@example.com>
2024-12-30 21:47:25 +0530

    fix: fixed mogodb connection

--- config/database.js
+++ config/database.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); 
 
 exports.connectDB = () => {
-  mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  mongoose.connect(process.env.DB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 };
